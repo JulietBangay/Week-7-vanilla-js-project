3780833456a05b23e8381df3056727433b763d8a
JulietBangay <julietabangay@gmail.com>
2021-12-02 10:10:27 +0000

    Added search current location and search city functions

--- src/script.js
+++ src/script.js
@@ -38,3 +38,84 @@ function currentDate() {
   time.innerHTML = `${hour}:${minutes}`;
 }
 currentDate();
+openingPageCurrentLocation();
+
+function openingPageCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+  function searchCurrentLocation(position) {
+    let apiKey = "f85062d84430cd35a6b8db439bd6c8f6";
+    let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(weatherApiUrl).then(displayWeatherCondition);
+    function displayWeatherCondition(response) {
+      console.log(response);
+      let city = document.querySelector("#current-city");
+      let latitude = Math.round(position.coords.latitude);
+      let longitude = Math.round(position.coords.longitude);
+      city.innerHTML = `${response.data.name} <br /> Latitude: ${latitude} <br /> Longitude: ${longitude}`;
+      let temperature = document.querySelector("#temperature");
+      temperature.innerHTML = Math.round(response.data.main.temp) + "°C";
+      let humidity = document.querySelector("#humidity");
+      humidity.innerHTML = response.data.main.humidity;
+      let windspeed = document.querySelector("#windspeed");
+      windspeed.innerHTML = Math.round(response.data.wind.speed);
+      let description = document.querySelector("#weather-description");
+      description.innerHTML = response.data.weather[0].main;
+    }
+  }
+}
+
+let currentLocationButton = document.querySelector(
+  "#search-current-location-button"
+);
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let searchCityButton = document.querySelector("#search-city-button");
+searchCityButton.addEventListener("click", searchCity);
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchCurrentLocation);
+}
+
+function searchCurrentLocation(position) {
+  let apiKey = "f85062d84430cd35a6b8db439bd6c8f6";
+  let weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(weatherApiUrl).then(displayWeatherCondition);
+  function displayWeatherCondition(response) {
+    console.log(response);
+    let city = document.querySelector("#current-city");
+    let latitude = Math.round(position.coords.latitude);
+    let longitude = Math.round(position.coords.longitude);
+    city.innerHTML = `${response.data.name} <br /> Latitude: ${latitude} <br /> Longitude: ${longitude}`;
+    let temperature = document.querySelector("#temperature");
+    temperature.innerHTML = Math.round(response.data.main.temp) + "°C";
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+    let windspeed = document.querySelector("#windspeed");
+    windspeed.innerHTML = Math.round(response.data.wind.speed);
+    let description = document.querySelector("#weather-description");
+    description.innerHTML = response.data.weather[0].main;
+  }
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-city");
+  cityInput.value = cityInput.value.toLowerCase().trim();
+  axios
+    .get(`${weatherApiUrl}q=${cityInput.value}&units=metric&appid=${apiKey}`)
+    .then(displayWeatherCondition);
+  function displayWeatherCondition(response) {
+    console.log(response);
+    let city = document.querySelector("#current-city");
+    city.innerHTML = `${response.data.name} `;
+    let temperature = document.querySelector("#temperature");
+    temperature.innerHTML = Math.round(response.data.main.temp) + "°C";
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+    let windspeed = document.querySelector("#windspeed");
+    windspeed.innerHTML = Math.round(response.data.wind.speed);
+    let description = document.querySelector("#weather-description");
+    description.innerHTML = response.data.weather[0].main;
+  }
+}
